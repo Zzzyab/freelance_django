7f782c4e3219a74c17c77210a13780e5d18ce50b
Zzzyab <ygolok_zyaba@mail.ru>
2020-07-30 20:49:59 +0500

    Пустой Джанго

--- oursite/oursite/urls.py
+++ oursite/oursite/urls.py
@@ -1,4 +1,4 @@
-"""myfirst URL Configuration
+"""oursite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
---
+++ valera_learn.py
@@ -0,0 +1,24 @@
+from random import choice
+
+int = 8 #как в луа
+float = 4.8 #как в луа
+
+random_strs = ["cock","dick","suck cock"]
+
+test = int + float
+
+boolean = True
+
+print(test)
+if boolean == True:
+    print( choice(random_strs) + "\n\"really\" nigga " + str(int) + str(float) )
+else:
+    print("fuck you you")
+
+
+# Если ты хочешь протестировать сайт то напиши в консоль
+# cd *путь к папке myfirst* к примеру у меня вот так будет  cd C:\Users\User\Desktop\python django\myfirst
+# потом пишешь C: или какой там у тебя диск
+# потом пишишье python manage.py runserver
+# заходишь на сайт http://127.0.0.1:8000/
+# готово
