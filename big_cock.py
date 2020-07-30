b6a3dd1b185214d7815bdaa20d2548d96289fdaf
Chocopieee <lenovosss585@gmail.com>
2020-07-30 15:40:48 +0500

    так эффектнее

--- big_cock.py
+++ big_cock.py
@@ -1,7 +1,9 @@
 from random import choice
+import time
 
 name = ["Валеряшка", "Семеняшка", "Алихашка", "Даниляшка", "Цоляшка", "Дауряшка", "Вовяшка"]
 status = ["Басота", "Урод", "Мужик", "Пробитый", "Лох", "Опущенный", "Валет", "Туз", "Смотрящий"]
 
 for namestatus in name:
     print( str( namestatus ) + " - " + str( choice(status) ) )
+    time.sleep(0.5)
