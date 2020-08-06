c34795500e997236aed50528e8357e87b0bbb526
Chocopieee <lenovosss585@gmail.com>
2020-08-06 16:01:52 +0500

    mosaert v0.0

--- otvet_zapadu/main/static/scripts/search.js
+++ otvet_zapadu/main/static/scripts/search.js
@@ -1,7 +1,6 @@
 
 function search() {
   var a = document.getElementById('Search').value;
-  console.log(a);
   if (a != '') {
     document.location.href = ("https://www.google.com/search?q="+a);
   }
