904c338a9b7414bd32e94d16ed1414c15551c08d
Chocopieee <lenovosss585@gmail.com>
2020-08-08 01:00:49 +0500

    ХААААААААЙП

--- mosaert/main/static/scripts/danil.js
+++ mosaert/main/static/scripts/danil.js
@@ -1,1 +1,21 @@
+var $ = jQuery;
 
+$(document).ready(function(){
+    $('.header').mouseover(function(){
+            $(".fakeheader").css({"top":"0", "left":"0"});
+            $(".nav_link").css({"color":"#88868B"});
+            $(".nav_link_ethics").css({"color":"#88868B"});
+            $(".nav_link_sales").css({"color":"#88868B"});
+        },
+
+    );
+
+    $('.header').on('mouseout', function(){
+            $(".fakeheader").css({"top":"-120px", "left":"0"});
+            $(".nav_link").css({"color":"#fff"});
+            $(".nav_link_ethics").css({"color":"#fff"});
+            $(".nav_link_sales").css({"color":"#fff"});
+        },
+
+    );
+});
