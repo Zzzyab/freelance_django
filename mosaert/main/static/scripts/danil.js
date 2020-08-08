ba6b56486d442e3696452aeba7175edb0ddc3692
Sagaph <FoxyKyH@mail.ru>
2020-08-09 01:43:46 +0300

    Доделал кнопку Sales ( почти )

--- mosaert/main/static/scripts/danil.js
+++ mosaert/main/static/scripts/danil.js
@@ -3,9 +3,12 @@ var $ = jQuery;
 $(document).ready(function(){
     $('.header').mouseover(function(){
             $(".fakeheader").css({"top":"0", "left":"0"});
-            $(".nav_link").css({"color":"#88868B"});
-            $(".nav_link_ethics").css({"color":"#88868B"});
-            $(".nav_link_sales").css({"color":"#88868B"});
+            $(".nav_link").css({"color":"#383838"});
+            $(".nav_link_ethics").css({"color":"#383838"});
+            $(".nav_link_sales").css({"color":"#DBB4AF",
+            "border":"2px solid #DBB4AF",
+            "border-radius":"20px",
+            "padding":"0px 15px"});
         },
 
     );
@@ -13,9 +16,11 @@ $(document).ready(function(){
     $('.header').on('mouseout', function(){
             $(".fakeheader").css({"top":"-120px", "left":"0"});
             $(".nav_link").css({"color":"#fff"});
-            $(".nav_link_ethics").css({"color":"#fff"});
-            $(".nav_link_sales").css({"color":"#fff"});
-        },
+            $(".nav_link_ethics").css({"color":"#fff"})
+            $(".nav_link_sales").css({"color":"#fff",
+            "border":"none",
+            "padding":"0px"})  
+        }
 
     );
 });
